786c1418e2548c27999aab45d6df032782953492
SirCodringher <107930727+SirCodringher@users.noreply.github.com>
2022-09-21 12:49:00 +0200

    Zadanie 10.4

---
+++ main.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, url_for, redirect, flash
+import tmdb_client
+import random
+import datetime
+
+
+app = Flask(__name__)
+
+FAVORITES = set()
+app.secret_key = b'my-secret'
+
+
+@app.route('/')
+def homepage():
+    type_list = ['top_rated', 'upcoming', 'popular', 'now_playing']
+    selected_list = request.args.get('list_type', 'popular')
+    if selected_list not in type_list:
+        selected_list = type_list[2]
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, selected_list=selected_list, type_list=type_list)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id, how_many=8)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
+
+
+@app.route("/search")
+def search():
+    search_query = request.args.get("q", "")
+    if search_query:
+        movies = tmdb_client.search(search_query=search_query)
+    else:
+        movies = []
+    return render_template("search.html", movies=movies, search_query=search_query)
+
+
+@app.route("/today")
+def today():
+    movies = tmdb_client.get_airing_today()
+    today = datetime.date.today()
+    return render_template("today.html", movies=movies, today=today)
+
+
+@app.route("/favorites/add", methods=["POST"])
+def add_to_favorites():
+    data = request.form
+    movie_id = data.get('movie_id')
+    movie_title = data.get('movie_title')
+    if movie_id and movie_title:
+        FAVORITES.add(movie_id)
+        flash(f"Dodano film {movie_title} do ulubionych!")
+    return redirect(url_for("homepage"))
+
+
+@app.route("/favorites")
+def show_favorites():
+    if FAVORITES:
+        movies = []
+        for movie_id in FAVORITES:
+            movie_details = tmdb_client.get_single_movie(movie_id)
+            movies.append(movie_details)
+    else:
+        movies = []
+    return render_template("homepage.html", movies=movies)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,69 @@
+import requests
+import random
+import os
+
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGE1YzMzNmM0ZmZhOTY4YzU5Mzk4MDM2NTg4YjY5YSIsInN1YiI6IjYzMjVmYjEwOWYxYmU3MDA3YzJiNTU2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uVXjBIBenhhtXXzp0o_wyUzL1NEkpisjTmiIVeGkgZM"
+
+def call_tmdb_api(endpoint):
+   full_url = f"https://api.themoviedb.org/3/{endpoint}"
+   headers = {
+       "Authorization": f"Bearer {api_token}"
+   }
+   response = requests.get(full_url, headers=headers)
+   response.raise_for_status()
+   return response.json()
+
+
+def get_movies_list(list_type='popular'):
+    return call_tmdb_api(f"movie/{list_type}")
+
+def get_poster_url(poster_api_path, size='342'):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    data = data['results']
+    random.shuffle(data)
+    return data[:how_many]
+
+def get_single_movie(movie_id):
+    return call_tmdb_api(f"movie/{movie_id}")
+
+
+def get_single_movie_cast(movie_id, how_many):
+    return call_tmdb_api(f"movie/{movie_id}/credits")['cast'][:how_many]
+
+
+def get_movie_images(movie_id):
+    return call_tmdb_api(f"movie/{movie_id}/images")
+
+
+def search(search_query):
+    base_url = "https://api.themoviedb.org/3/"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    endpoint = f"{base_url}search/movie/?query={search_query}"
+    response = requests.get(endpoint, headers=headers)
+    response = response.json()
+    return response['results']
+
+
+def get_airing_today():
+    endpoint = "https://api.themoviedb.org/3/tv/airing_today"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    response = response.json()
+    return response['results']
+
+
+
+
+
+
+
+
